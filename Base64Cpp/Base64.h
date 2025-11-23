94014e3565619c83593382166624288d182e8534
madelmann <madelmann@users.noreply.github.com>
2025-11-23 11:07:20 +0100

    added LICENSE and .gitignore

--- Base64Cpp/Base64.h
+++ Base64Cpp/Base64.h
@@ -7,7 +7,7 @@
 class Base64
 {
 public:
-    static std::string decode( const std::string& code, bool removeLineBreaks ); 
+    static std::string decode( const std::string& code, bool removeLineBreaks );
     static std::string encode( const std::string& code, bool url );
 };
 
